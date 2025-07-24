f3f58ddd1995b374f745f692a832be85188be6c9
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-07-24 20:43:13 +0800

    add useEffect to fetch the meme api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Main = () => {
-    const [meme, setMeme] = useState({
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
-    })
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    const handleChange = (event) => {
-        const {value,name} = event.currentTarget;
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
-    }
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+  },[])
+
+  const memeImage = () => {
+    const randomNum = Math.floor(Math.random() * allMemes.length)
+    const newMemeUrl = allMemes[randomNum].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      imageUrl: newMemeUrl
+    }))
+  }
+
+  const handleChange = (event) => {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
   return (
     <main>
       <div className="form">
         <label htmlFor="topText">
           Top Text
-          <input type="text" name="topText" value={meme.topText} onChange={handleChange}/>
+          <input
+            type="text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor="bottomText">
           Bottom Text
-          <input type="text" name="bottomText" value={meme.bottomText} onChange={handleChange} />
+          <input
+            type="text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={memeImage}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.imageUrl} alt="meme-image" />
